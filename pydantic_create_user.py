536429c34b017027b328676efc5557a16d85c8f7
viktor96228 <vievpi@mail.ru>
2025-10-05 21:13:51 +0900

    Знакомство с Pydantic

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -1,23 +1,31 @@
 from pydantic import BaseModel, Field, EmailStr
 
-# модель UserSchema
-class UserSchema(BaseModel):
-    id: str
-    email: EmailStr  # Используем EmailStr вместо str
-    last_name: str = Field(alias="lastName")
-    first_name: str = Field(alias="firstName")
-    middle_name: str = Field(alias="middleName")
-    phone_number: str = Field(alias="phoneNumber")
 
-# модель CreateUserRequestSchema
-class CreateUserRequestSchema(BaseModel):
+class UserSchema(BaseModel):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
     email: EmailStr
     last_name: str = Field(alias="lastName")
     first_name: str = Field(alias="firstName")
     middle_name: str = Field(alias="middleName")
     phone_number: str = Field(alias="phoneNumber")
 
-# модель CreateUserRequestSchema
-class CreateUserResponseSchema(BaseModel):
-    user: UserSchema
 
+class CreateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: UserSchema
