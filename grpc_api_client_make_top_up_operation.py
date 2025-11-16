c6109d4b389fcd4b4d9f3efdde7f39e5425197bb
viktor96228 <vievpi@mail.ru>
2025-11-16 16:19:59 +0900

    Практикуемся в использовании gRPC API клиентов: создание операции пополнения счета

---
+++ grpc_api_client_make_top_up_operation.py
@@ -0,0 +1,26 @@
+# Импортируем фабричные функции для создания API-клиентов
+from clients.grpc.gateway.users.client import build_users_gateway_grpc_client
+from clients.grpc.gateway.accounts.client import build_accounts_gateway_grpc_client
+from clients.grpc.gateway.operations.client import build_operations_gateway_grpc_client
+
+# Создаём API-клиенты для работы с сервисами Users, Accounts и Operations
+users_gateway_client = build_users_gateway_grpc_client()
+accounts_gateway_client = build_accounts_gateway_grpc_client()
+operations_gateway_client = build_operations_gateway_grpc_client()
+
+# Создаем пользователя
+create_user_response = users_gateway_client.create_user()
+print('Create user response:', create_user_response)
+
+# Открываем дебетовый счет
+open_debit_card_account_response = accounts_gateway_client.open_debit_card_account(
+    user_id=create_user_response.user.id
+)
+print('Open debit card account response:', open_debit_card_account_response)
+
+# Создаём операцию пополнения счёта
+make_top_up_operation_response = operations_gateway_client.make_top_up_operation(
+    card_id=open_debit_card_account_response.account.cards[0].id,
+    account_id=open_debit_card_account_response.account.id
+)
+print('Make top up operation response:', make_top_up_operation_response)
