30c765b91a9c1b28af2c55058ce0c9e03d320a0e
viktor96228 <vievpi@mail.ru>
2025-12-04 00:50:00 +0900

    Подготовка gRPC API клиента для Locust

--- clients/grpc/gateway/users/client.py
+++ clients/grpc/gateway/users/client.py
@@ -1,12 +1,15 @@
 from grpc import Channel
+from locust.env import Environment  # Импорт окружения Locust
 
 from clients.grpc.client import GRPCClient
-from clients.grpc.gateway.client import build_gateway_grpc_client
+from clients.grpc.gateway.client import (
+    build_gateway_grpc_client,
+    build_gateway_locust_grpc_client  # Импорт билдера для нагрузочного тестирования
+)
 from contracts.services.gateway.users.rpc_create_user_pb2 import CreateUserRequest, CreateUserResponse
 from contracts.services.gateway.users.rpc_get_user_pb2 import GetUserRequest, GetUserResponse
 from contracts.services.gateway.users.users_gateway_service_pb2_grpc import UsersGatewayServiceStub
 from tools.fakers import fake
-
 
 class UsersGatewayGRPCClient(GRPCClient):
     """
@@ -75,3 +78,16 @@ def build_users_gateway_grpc_client() -> UsersGatewayGRPCClient:
     :return: Инициализированный клиент для UsersGatewayService.
     """
     return UsersGatewayGRPCClient(channel=build_gateway_grpc_client())
+
+# Новый билдер для нагрузочного тестирования
+def build_users_gateway_locust_grpc_client(environment: Environment) -> UsersGatewayGRPCClient:
+    """
+    Функция создаёт экземпляр UsersGatewayGRPCClient адаптированного под Locust.
+
+    Клиент автоматически собирает метрики и передаёт их в Locust через хуки.
+    Используется исключительно в нагрузочных тестах.
+
+    :param environment: объект окружения Locust.
+    :return: экземпляр UsersGatewayGRPCClient с хуками сбора метрик.
+    """
+    return UsersGatewayGRPCClient(channel=build_gateway_locust_grpc_client(environment))
