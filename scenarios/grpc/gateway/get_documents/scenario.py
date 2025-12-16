96839ee217d7714e82d7699b18862018fabe837a
viktor96228 <vievpi@mail.ru>
2025-12-16 23:26:23 +0900

    Рефакторинг и улучшение нагрузочных тестов

--- scenarios/grpc/gateway/get_documents/scenario.py
+++ scenarios/grpc/gateway/get_documents/scenario.py
@@ -1,8 +1,10 @@
-from locust import User, between, task
+from locust import task
 
 from clients.grpc.gateway.locust import GatewayGRPCSequentialTaskSet
 from contracts.services.gateway.accounts.rpc_open_savings_account_pb2 import OpenSavingsAccountResponse
 from contracts.services.gateway.users.rpc_create_user_pb2 import CreateUserResponse
+from tools.locust.user import LocustBaseUser  # Импортируем базовый класс
+
 
 
 class GetDocumentsSequentialTaskSet(GatewayGRPCSequentialTaskSet):
@@ -54,10 +56,8 @@ class GetDocumentsSequentialTaskSet(GatewayGRPCSequentialTaskSet):
         )
 
 
-class GetDocumentsScenarioUser(User):
+class GetDocumentsScenarioUser(LocustBaseUser):  # Наследуем от базового класса
     """
     Пользователь Locust, исполняющий последовательный сценарий получения документов.
     """
-    host = "localhost"
-    tasks = [GetDocumentsSequentialTaskSet]
-    wait_time = between(1, 3)
+    tasks = [GetDocumentsSequentialTaskSet]  # Указываем только задачи
--- scenarios/http/gateway/get_documents/scenario.py
+++ scenarios/http/gateway/get_documents/scenario.py
@@ -1,8 +1,11 @@
-from locust import User, between, task
+from locust import task
 
 from clients.http.gateway.accounts.schema import OpenSavingsAccountResponseSchema
 from clients.http.gateway.locust import GatewayHTTPSequentialTaskSet
 from clients.http.gateway.users.schema import CreateUserResponseSchema
+from tools.locust.user import LocustBaseUser  # Импортируем базовый класс
+
+
 
 
 class GetDocumentsSequentialTaskSet(GatewayHTTPSequentialTaskSet):
@@ -54,10 +57,9 @@ class GetDocumentsSequentialTaskSet(GatewayHTTPSequentialTaskSet):
         )
 
 
-class GetDocumentsScenarioUser(User):
+class GetDocumentsScenarioUser(LocustBaseUser):    # Наследуем от базового класса
     """
     Пользователь Locust, исполняющий последовательный сценарий получения документов.
     """
-    host = "localhost"
-    tasks = [GetDocumentsSequentialTaskSet]
-    wait_time = between(1, 3)
+    tasks = [GetDocumentsSequentialTaskSet]   # Указываем только задачи
+
