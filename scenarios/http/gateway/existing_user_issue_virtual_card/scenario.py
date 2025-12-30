b249305b9e6fcc68ffec6dfb1493d0045f175162
viktor96228 <vievpi@mail.ru>
2025-12-31 00:24:26 +0900

    Практика: Выпуск виртуальной карты существующим пользователем

---
+++ scenarios/http/gateway/existing_user_issue_virtual_card/scenario.py
@@ -0,0 +1,53 @@
+from locust import task, events
+from locust.env import Environment
+
+from clients.http.gateway.locust import GatewayHTTPTaskSet
+from seeds.scenarios.existing_user_issue_virtual_card import ExistingUserIssueVirtualCardSeedsScenario
+from seeds.schema.result import SeedUserResult
+from tools.locust.user import LocustBaseUser
+
+
+# Этот хук выполняется один раз при инициализации теста (до старта пользователей).
+# Мы используем его, чтобы заранее прогнать сидинг и загрузить пользователей в память.
+@events.init.add_listener
+def init(environment: Environment, **kwargs):
+    # Создаем экземпляр сидинг-сценария
+    seeds_scenario = ExistingUserIssueVirtualCardSeedsScenario()
+
+    # Выполняем генерацию данных, если они ещё не созданы
+    seeds_scenario.build()
+
+    # Загружаем сгенерированных пользователей в окружение Locust
+    environment.seeds = seeds_scenario.load()
+
+
+# Набор задач (TaskSet), который будет выполняться виртуальными пользователями.
+class IssueVirtualCardTaskSet(GatewayHTTPTaskSet):
+    # Типизируем объект пользователя из сидинга
+    seed_user = SeedUserResult
+
+    # Метод вызывается при запуске каждой сессии пользователя (до начала задач)
+    def on_start(self) -> None:
+        super().on_start()
+
+        # Получаем следующего пользователя из списка (по порядку!)
+        self.seed_user = self.user.environment.seeds.get_next_user()
+
+    @task(4)
+    def get_accounts(self):
+        # Запрашиваем список счетов
+        self.accounts_gateway_client.get_accounts(user_id=self.seed_user.user_id)
+
+    @task(1)
+    def issue_virtual_card(self):
+        # Выпуск новой виртуальной карты.
+        self.cards_gateway_client.issue_virtual_card(
+            user_id=self.seed_user.user_id,
+            account_id=self.seed_user.debit_card_accounts[0].account_id
+        )
+
+g
+# Конкретный пользовательский класс, который будет запускать наш TaskSet
+class IssueVirtualCardScenarioUser(LocustBaseUser):
+    tasks = [IssueVirtualCardTaskSet]
+
